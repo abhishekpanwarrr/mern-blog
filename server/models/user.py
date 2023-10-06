d09a8d310540f9605e583a8753005342d6750505
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-06 12:59:28 +0530

    added all user routes

--- server/models/user.py
+++ server/models/user.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel
 
 
@@ -13,3 +14,12 @@ class User(BaseModel):
 class LoginUser(BaseModel):
     email: str
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
--- server/routes/users.py
+++ server/routes/users.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Depends
 from models.user import User, LoginUser
 from database.database import db
 from schemas.schemas import list_serial
 from bson import ObjectId
 from pymongo.errors import PyMongoError
+from hashing import Hash
+from jwttoken import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
 
 collection = db["users"]
 
@@ -15,10 +18,10 @@ async def login(user: LoginUser):
     try:
         result = collection.find_one({"email": user.email})
         if result:
-            # Remove the 'password' and '_id' fields to make the result JSON-serializable
-            result.pop("password", None)
-            result.pop("_id", None)
-            return {"message": "User found", "user": result}
+            if not Hash.verify(result["password"], user.password):
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+            access_token = create_access_token(data={"sub": user.email})
+            return {"access_token": access_token, "token_type": "bearer"}
         else:
             return {"message": "User not found"}
     except PyMongoError as e:
@@ -35,7 +38,11 @@ async def login(user: LoginUser):
 @router.post("/register", status_code=status.HTTP_200_OK)
 async def register(user: User):
     try:
-        result = collection.insert_one(dict(user))
+        hashed_pass = Hash.bcrypt(user.password)
+        user.password = hashed_pass
+        user_dict = user.__dict__
+        user_dict.pop("__dict__", None)
+        result = collection.insert_one(user_dict)
         inserted_id = str(result.inserted_id)
         return {"message": "User created successfully", "inserted_id": inserted_id}
     except PyMongoError as e:
@@ -47,3 +54,14 @@ async def register(user: User):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Something went wrong: {str(e)}",
         )
+
+
+# @router.post("/login")
+# def login(request: OAuth2PasswordRequestForm = Depends()):
+#     user = db["users"].find_one({"username": request.username})
+#     if not user:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+# if not Hash.verify(user["password"], request.password):
+#     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+# access_token = create_access_token(data={"sub": user["username"]})
+# return {"access_token": access_token, "token_type": "bearer"}
