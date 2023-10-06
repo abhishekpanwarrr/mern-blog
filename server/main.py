8f4a9c297d2f99a46de5b8418317c8379a3d16f9
Abhishek <40629087+abhishekpanwarrr@users.noreply.github.com>
2023-10-06 12:59:19 +0530

    added hasing to password

--- server/main.py
+++ server/main.py
@@ -3,15 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from routes.post import router as postRouter
 from routes.users import router as userRouter
+
 app = FastAPI()
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router=postRouter)
 app.include_router(router=userRouter)
